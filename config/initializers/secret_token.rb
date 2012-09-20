67386ceca7feca28461e1b899f796e10f8289c09
Marc Lambert <marclambertagas@gmail.com>
2012-09-20 13:01:37 +0800

    Initial files for vmobile project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vmobile::Application.config.secret_token = '6adb152fdb0152969419f930a62c0ea8f6c4834c5ad91e54798ca8e4ccda358642a073116bacc86c6a2de91e3cffbdeedc1e9fa2ce5b62b14fadf5fc6c9a5a37'
